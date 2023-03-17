a22124f37e214b6af1d0ac828c7ec77ba7d3a819
JosephGbomiye <adegokejoseph777@gmail.com>
2023-03-17 16:36:27 -0700

    Upper and Lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This program prints both lowercase and uppercase'
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	/* the character to be written*/
+	char upper_case = 'A';
+	char lower_case = 'a';
+	/* write the character to stdout */
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+		putchar (upper_case);
+	putchar('\n');
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+		putchar (lower_case);
+	putchar('\n');
+
+	return (0);
+}
