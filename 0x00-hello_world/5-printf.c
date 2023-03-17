08f0adaef8ebc5fee544ef9b828c6c84ac5cce5a
JosephGbomiye <adegokejoseph777@gmail.com>
2023-03-16 18:16:08 -0700

    update printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - A C program that prints
- * * exactly with proper grammar, but the outcome is a piece of art.
- * followed by a new line.
- * Return: always 0 (Success)
+ * main - The purpose of this file is to output
+ * the string: with proper grammar, but the outcome is a piece of art,
+ * followed by a newline, using printf and not puts.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
+}
