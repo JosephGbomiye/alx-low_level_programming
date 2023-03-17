e13a32606dd82f102dfc57c5c19e37c0ada42318
JosephGbomiye <adegokejoseph777@gmail.com>
2023-03-17 16:09:49 -0700

    A-Z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This program prints alphabets in lower cases'
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	/* the character to be written */
+	char lower_Case = 'a';
+	/* write the character to stdout */
+	for (lower_Case = 'a'; lower_Case <= 'z'; lower_Case++)
+		putchar(lower_Case);
+
+	putchar('\n');
+
+	return (0);
+}
