22a1bcc41e564f8f7b3cfb351d24a8d368a244df
JosephGbomiye <adegokejoseph777@gmail.com>
2023-03-17 17:00:01 -0700

    PL

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ int main(void)
 	char upper_case = 'A';
 	char lower_case = 'a';
 	/* write the character to stdout */
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-		putchar (upper_case);
-
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 		putchar (lower_case);
+
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+		putchar (upper_case);
 	putchar('\n');
 
 	return (0);
