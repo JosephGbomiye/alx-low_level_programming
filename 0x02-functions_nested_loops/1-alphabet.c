c397a19ad4dc8959184224d889e43c607a7ac0bd
JosephGbomiye <adegokejoseph777@gmail.com>
2023-03-21 07:30:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphanet - Make the alphanet
+ * Return void
+ */
+
+void print_alphanet(void);
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
